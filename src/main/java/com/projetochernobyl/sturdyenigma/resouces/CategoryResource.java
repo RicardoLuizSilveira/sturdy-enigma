f56824f499b870efae9b669740cc620b85104e3e
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2020-05-11 17:39:27 -0300

    Test first domain class - Category

--- src/main/java/com/projetochernobyl/sturdyenigma/resouces/CategoryResource.java
+++ src/main/java/com/projetochernobyl/sturdyenigma/resouces/CategoryResource.java
@@ -1,16 +1,23 @@
 package com.projetochernobyl.sturdyenigma.resouces;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projetochernobyl.sturdyenigma.domain.Category;
 
 @RestController
 @RequestMapping(value = "/api/v1/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String list() {
-		return "REST is working";
+	public List<Category> list() {
+		Category cat1 = new Category(1L, "Informática");
+		Category cat2 = new Category(2L, "Escritório");
+		return Arrays.asList(cat1, cat2);
 	}
 	
 }
