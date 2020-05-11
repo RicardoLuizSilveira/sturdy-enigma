e70b29d26ac2dad4629c5790de50a2fd311a325f
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2020-05-11 16:10:32 -0300

    Creating project

---
+++ src/main/java/com/projetochernobyl/sturdyenigma/SturdyEnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.projetochernobyl.sturdyenigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SturdyEnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SturdyEnigmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetochernobyl/sturdyenigma/SturdyEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetochernobyl.sturdyenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturdyEnigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
