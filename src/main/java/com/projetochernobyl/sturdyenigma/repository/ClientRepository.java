411c7fc23cd7477c0fd32c1fd129eac9e90e26c2
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2020-05-16 18:50:32 -0300

    Email validation

--- src/main/java/com/projetochernobyl/sturdyenigma/repository/ClientRepository.java
+++ src/main/java/com/projetochernobyl/sturdyenigma/repository/ClientRepository.java
@@ -1,11 +1,17 @@
 package com.projetochernobyl.sturdyenigma.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetochernobyl.sturdyenigma.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
-
+	
+	@Transactional(readOnly = true)
+	Optional<Client> findByEmail(String email);
+	
 }
--- src/main/java/com/projetochernobyl/sturdyenigma/services/validation/ClientInsertValidator.java
+++ src/main/java/com/projetochernobyl/sturdyenigma/services/validation/ClientInsertValidator.java
@@ -2,16 +2,24 @@ package com.projetochernobyl.sturdyenigma.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.projetochernobyl.sturdyenigma.domain.Client;
 import com.projetochernobyl.sturdyenigma.domain.enums.ClientType;
 import com.projetochernobyl.sturdyenigma.dto.ClientNewDTO;
+import com.projetochernobyl.sturdyenigma.repository.ClientRepository;
 import com.projetochernobyl.sturdyenigma.resouces.exceptions.FieldMessage;
 import com.projetochernobyl.sturdyenigma.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	
+	@Autowired
+	private ClientRepository repo;
 
 	@Override
 	public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
@@ -28,6 +36,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 		}
 		if (objDto.getType() == null) {
 			list.add(new FieldMessage("type", "Field should not be empty"));
+		}
+		
+		Optional<Client> opt = repo.findByEmail(objDto.getEmail());
+		if (opt.isPresent()) {
+			list.add(new FieldMessage("email", "This field already exist"));
 		}
 		
 		for (FieldMessage e : list) {
